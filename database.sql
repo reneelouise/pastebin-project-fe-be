3e8697f36e8dcab72b7d39685be146b1cba907de
Renee-Louise Nzegbulem <renee@Renee-Louises-MacBook-Pro.local>
2021-12-06 16:01:27 +0000

    database created

---
+++ database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE rbgdatabase;
+
+CREATE TABLE pastes( pastes_id SERIAL PRIMARY KEY, pastes VARCHAR(1000) );
+
+
+INSERT INTO pastes(pastes) VALUES ('Real change enduring change happens one step at a time.');
--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import { Client } from "pg";
-import { config } from "dotenv";
+// import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
 
-config(); //Read .env file lines as though they were env vars.
+const pool = require("./db");
+
+
+// config(); //Read .env file lines as though they were env vars.
 
 //Call this script with the environment variable LOCAL set if you want to connect to a local db (i.e. without SSL)
 //Do not set the environment variable LOCAL if you want to connect to a heroku DB.
@@ -11,6 +14,7 @@ config(); //Read .env file lines as though they were env vars.
 //For the ssl property of the DB connection config, use a value of...
 // false - when connecting to a local DB
 // { rejectUnauthorized: false } - when connecting to a heroku DB
+const PORT = 4000;
 const herokuSSLSetting = { rejectUnauthorized: false }
 const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
 const dbConfig = {
@@ -26,14 +30,37 @@ app.use(cors()) //add CORS support to each following route handler
 const client = new Client(dbConfig);
 client.connect();
 
+// //post req
+// app.post("/", async (req, res) => {
+//   const dbres = await client.query('select * from pastes');
+//   try {
+//     console.log("Hello I am working!")
+    
+//   } catch (err) {
+//     console.error(err.message)
+    
+//   }
+
+//   // res.json(dbres.rows);
+// });
+
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
-  res.json(dbres.rows);
+  
+  try {
+    const dbres = await pool.query('SELECT * FROM pastes');
+    res.json(dbres.rows);
+    
+  } catch (err) {
+    console.error(err.message)
+    
+  }
+
+
 });
 
 
 //Start the server on the given port
-const port = process.env.PORT;
+const port = PORT
 if (!port) {
   throw 'Missing PORT environment variable.  Set it in .env file.';
 }
